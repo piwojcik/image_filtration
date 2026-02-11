b9df70d7e079d947f9f4e5e25231e84b6d120f02
piwojcik <piwojcik@student.agh.edu.pl>
2026-02-11 03:21:43 +0100

    rgb2gray

---
+++ Zybo_Z7/project_1/project_1.srcs/sources_1/new/rgb2gray.sv
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/06/2026 11:05:43 PM
+// Design Name: 
+// Module Name: rgb2gray
+// Project Name: Image filtration
+// Target Devices: 
+// Tool Versions: 
+// Description: Converts 24-bit RGB data to 8-bit grayscale before filtration
+// 
+// Dependencies: RGB to Grayscale Converter uses ITU-R BT.601 standard
+// 
+// revision:
+// revision 0.01 - File Created
+// Additional Comments: ITU-R BT.601 standard: Y = 0.299*R + 0.587*G + 0.114*B
+//                      For simple hardware implementation used bit-shifting, and approximated coefficients:
+//                      Y = 0.281*R + 0.562*G + 0.093*B
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rgb2gray(
+    input  logic        clk,
+    input  logic        rst,
+    input  logic        hsync_in,
+    input  logic        vsync_in,
+    input  logic        vde_in,
+    
+    input  logic [23:0] rgb_in,
+    
+    output logic        hsync_o,
+    output logic        vsync_o,
+    output logic        vde_o,
+    
+    output logic [7:0] gray_o
+);
+    
+    always_ff @(posedge clk) begin
+      if(rst) begin
+        hsync_o <= '0;
+        vsync_o <= '0;
+        vde_o   <= '0;                 
+        gray_o  <= '0;
+      end else begin
+        hsync_o <= hsync_in;
+        vsync_o <= vsync_in;
+        vde_o   <= vde_in;                 
+        gray_o  <= calc_gray;        
+      end
+    end
+
+logic [7:0] r, g, b, r1, r2, g1, g2, b1, b2, calc_gray;    
+//    coefficients r * 0,281 + g * 0,562 + b * 0,093
+    always_comb begin
+      r = rgb_in[23:16];
+      g = rgb_in[15:8];
+      b = rgb_in[7:0];
+
+      r1 = r >> 2;
+      r2 = r >> 5;
+
+      g1 = g >> 1;
+      g2 = g >> 4;
+     
+      b1 = b >> 4;
+      b2 = b >> 5;   
+      
+      calc_gray = r1+r2+g1+g2+b1+b2;
+    end
+endmodule
--- Zybo_Z7/project_1/project_1.srcs/sources_1/new/top.sv
+++ Zybo_Z7/project_1/project_1.srcs/sources_1/new/top.sv
@@ -21,37 +21,42 @@
 
 module top(
     input  logic sys_clk_125mhz,
-    input  logic sys_rst_n,
-    output  logic hdmi_rx_hpd,
+    input  logic sys_rst,
+
     inout  wire hdmi_rx_scl,
     inout  wire hdmi_rx_sda,
     input  logic hdmi_rx_clk_p,
     input  logic hdmi_rx_clk_n,
     input  logic [2:0] hdmi_rx_data_n,
     input  logic [2:0] hdmi_rx_data_p,
+    output  logic hdmi_rx_hpd,
+
     output logic hdmi_tx_clk_p,
     output logic hdmi_tx_clk_n,
     output logic [2:0] hdmi_tx_data_n,
     output logic [2:0] hdmi_tx_data_p,
+
     output logic led[3:0],
     input logic sw[2:0]
 );        
-logic [23:0] data_in, data_reg;
+
+logic [23:0] rgb_data, data_reg;
+logic [7:0] gray_data;
 logic video_active_reg, hsync_reg, vsync_reg, PixelClk_o;
 logic video_active_in, hsync_in, vsync_in;
 
 assign hdmi_rx_hpd = 1'b1;
 
-always_ff @(posedge PixelClk_o) begin
-    hsync_reg <= hsync_in;
-    vsync_reg <= vsync_in;
-    video_active_reg <= video_active_in;
-    if(sw[1])begin
-        data_reg  <= 24'hFFFFFF;
-    end else begin
-        data_reg <= data_in;
-    end
-end
+// always_ff @(posedge PixelClk_o) begin
+//     hsync_reg <= hsync_in;
+//     vsync_reg <= vsync_in;
+//     video_active_reg <= video_active_in;
+//     if(sw[1])begin
+//         data_reg  <= 24'hFFFFFF;
+//     end else begin
+//         data_reg <= rgb_data;
+//     end
+// end
     
 design_1_wrapper u_design_1_wrapper(
     .TMDS_0_clk_n(hdmi_rx_clk_n),
@@ -64,18 +69,39 @@ design_1_wrapper u_design_1_wrapper(
     .TMDS_1_data_p(hdmi_tx_data_p),
     .hdmi_in_ddc_scl_io(hdmi_rx_scl),
     .hdmi_in_ddc_sda_io(hdmi_rx_sda),
+
     .pLocked_0(led[1]),
-    .reset_rtl(sw[0]),
+    .reset_rtl(sys_rst),
     .sys_clock(sys_clk_125mhz),
     .PixelClk(PixelClk_o),  
-    .RGB2dvi_active_video(video_active_in),
-    .RGB2dvi_data(data_in),
-    .RGB2dvi_hsync(hync_in),
-    .RGB2dvi_vsync(vsync_in),
+    // HDMI IN to RGB
     .dvi2RGB_active_video(video_active_in),
-    .dvi2RGB_data(data_in),
+    .dvi2RGB_data(rgb_data),
     .dvi2RGB_hsync(hsync_in),
-    .dvi2RGB_vsync(vsync_in)
+    .dvi2RGB_vsync(vsync_in),
+    // RGB to HDMI OUT
+    .RGB2dvi_active_video(video_active_reg),
+    .RGB2dvi_data({gray_data,gray_data,gray_data}),
+    .RGB2dvi_hsync(hsync_reg),
+    .RGB2dvi_vsync(vsync_reg)
     );
     
+assign video_active_reg = led[0];
+assign led[2] = sys_rst;
+
+    rgb2gray u_rgb2gray( 
+    .clk(PixelClk_o),
+    .rst(sys_rst),
+    .hsync_in(hsync_in),
+    .vsync_in(vsync_in),
+    .vde_in(video_active_in),
+    
+    .rgb_in(rgb_data),
+    
+    .hsync_o(hsync_reg),
+    .vsync_o(vsync_reg),
+    .vde_o(video_active_reg),
+    
+    .gray_o(gray_data)
+);
 endmodule
